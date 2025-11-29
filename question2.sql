196cc611ea78acfc288637a24a044202657c2c02
JK-22025 <kazanj7@gmail.com>
2025-11-29 14:27:08 -0500

    Minor change to question 2

--- question2.sql
+++ question2.sql
@@ -1,3 +1,4 @@
+-- Assigned to : Jawad
 -- Question 2: 
 -- a) By using SELECT .. BULK COLLECT .., type a plsql program that asks to enter a city id and display all planes based in this city (pla_id, desc, max_passenger and city name)     
 -- b) Execute this code for the cities : 102, 101
